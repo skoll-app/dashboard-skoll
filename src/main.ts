ba5c60b74f6a630178bfb927f9fc2bb4715dad13
Adan manjaro <adan.florezb@gmail.com>
2022-08-03 19:55:25 -0500

    feat: primevue was installed

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,20 @@ import { createPinia } from "pinia";
 import App from "./App.vue";
 import router from "./router";
 
-import "./assets/main.css";
+import "primevue/resources/themes/saga-orange/theme.css";
+import "primevue/resources/primevue.min.css";
+import "primeicons/primeicons.css";
+// Primevue Components
+import PrimeVue from "primevue/config";
+import Button from "primevue/button";
 
 const app = createApp(App);
 
+app.use(PrimeVue);
 app.use(createPinia());
 app.use(router);
+
+// eslint-disable-next-line vue/multi-word-component-names, vue/no-reserved-component-names
+app.component("Button", Button);
 
 app.mount("#app");
