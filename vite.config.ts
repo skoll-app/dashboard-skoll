5e8ea061d9e02e6bb3993ab1f50d88cd3a912528
Adan manjaro <adan.florezb@gmail.com>
2022-08-06 20:05:25 -0500

    gh pages added

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: process.env.NODE_ENV === "development" ? "" : "/dashboard-skoll/",
   plugins: [
     vue(),
     vueI18n({
