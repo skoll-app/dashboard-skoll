1e6fb76155f81068fa3b801c30a916d42addd21a
Adan manjaro <adan.florezb@gmail.com>
2022-08-22 01:29:02 -0500

    feat: business login

---
+++ src/interfaces/business.ts
@@ -0,0 +1,10 @@
+export interface Business {
+  id: number;
+  apiKey: string;
+  apiLogin: string;
+  frontLogo?: string;
+  logo?: string;
+  status: boolean;
+  name: string;
+  dateCreate: string;
+}
