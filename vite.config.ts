0a4f30d60d94b313088a2a372a9d2e4addd4c8c0
Adan manjaro <adan.florezb@gmail.com>
2022-08-30 10:32:07 -0500

    base url hardcoded

--- vite.config.ts
+++ vite.config.ts
@@ -9,7 +9,9 @@ import vueI18n from "@intlify/vite-plugin-vue-i18n";
 export default defineConfig({
   base: process.env.NODE_ENV === "production" ? "/dashboard-skoll/" : "",
   define: {
-    "process.env": process.env,
+    "process.env": {
+      VITE_APP_BASE_URL: "https://0d6d-181-237-17-152.ngrok.io",
+    },
   },
   plugins: [
     vue(),
