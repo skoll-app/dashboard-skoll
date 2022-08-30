5f834b70d990096b798af9b8aec03b69ce42e164
Adan manjaro <adan.florezb@gmail.com>
2022-08-29 22:10:35 -0500

    feat: upload files

--- src/views/onboarding/steps/DocumentsView.vue
+++ src/views/onboarding/steps/DocumentsView.vue
@@ -1,7 +1,94 @@
-<template>
-  <div>Documentos</div>
-</template>
+<script setup lang="ts">
+import type { FileUploadRemoveEvent } from "primevue/fileupload";
+import { useI18n } from "vue-i18n";
 
-<script setup lang="ts"></script>
+const { t } = useI18n();
+const myUploader = (event: FileUploadRemoveEvent) => {
+  console.log(event.files);
+};
+</script>
+<template>
+  <Card class="mb-4">
+    <template v-slot:title>
+      {{ t("onboarding.managerLegalInfo") }}
+    </template>
+    <template v-slot:subtitle>
+      {{ t("onboarding.bank.document") }}
+    </template>
+    <template v-slot:content>
+      <FileUpload
+        name="demo[]"
+        url="./upload.php"
+        customUpload
+        accept="image/*"
+        :maxFileSize="1000000"
+        :upload-label="t('form.buttons.upload')"
+        :cancel-label="t('form.buttons.cancel')"
+        :choose-label="t('form.buttons.select')"
+        :multiple="false"
+        :fileLimit="1"
+        @uploader="myUploader"
+      >
+        <template #empty>
+          <p>{{ t("form.dragAndDrop") }}</p>
+        </template>
+      </FileUpload>
+    </template>
+  </Card>
+  <Card class="mb-4">
+    <template v-slot:title>
+      {{ t("onboarding.bank.certificate.title") }}
+    </template>
+    <template v-slot:subtitle>
+      {{ t("onboarding.bank.certificate.subtitle") }}
+    </template>
+    <template v-slot:content>
+      <FileUpload
+        name="demo[]"
+        url="./upload.php"
+        customUpload
+        accept="image/*"
+        :maxFileSize="1000000"
+        :upload-label="t('form.buttons.upload')"
+        :cancel-label="t('form.buttons.cancel')"
+        :choose-label="t('form.buttons.select')"
+        :multiple="false"
+        :fileLimit="1"
+        @uploader="myUploader"
+      >
+        <template #empty>
+          <p>{{ t("form.dragAndDrop") }}</p>
+        </template>
+      </FileUpload>
+    </template>
+  </Card>
+  <Card>
+    <template v-slot:title>
+      {{ t("onboarding.bank.rut.title") }}
+    </template>
+    <template v-slot:subtitle>
+      <div v-html="t('onboarding.bank.rut.subtitle')"></div>
+    </template>
+    <template v-slot:content>
+      <FileUpload
+        name="demo[]"
+        url="./upload.php"
+        customUpload
+        accept="image/*"
+        :maxFileSize="1000000"
+        :upload-label="t('form.buttons.upload')"
+        :cancel-label="t('form.buttons.cancel')"
+        :choose-label="t('form.buttons.select')"
+        :multiple="false"
+        :fileLimit="1"
+        @uploader="myUploader"
+      >
+        <template #empty>
+          <p>{{ t("form.dragAndDrop") }}</p>
+        </template>
+      </FileUpload>
+    </template>
+  </Card>
+</template>
 
 <style scoped></style>
