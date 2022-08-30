2d85ba1101bc6cd203839011ccc0f250bfc83251
Adan manjaro <adan.florezb@gmail.com>
2022-08-30 11:36:06 -0500

    fix: conditional config

--- vite.config.ts
+++ vite.config.ts
@@ -6,11 +6,12 @@ import vue from "@vitejs/plugin-vue";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
+export default defineConfig(({ mode, command }) => {
   return {
     base: process.env.NODE_ENV === "production" ? "/dashboard-skoll/" : "",
     define: {
-      "process.env": loadEnv(mode, process.cwd()),
+      "process.env":
+        command === "serve" ? loadEnv(mode, process.cwd()) : process.env,
     },
     plugins: [
       vue(),
