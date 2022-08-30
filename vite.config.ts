0903321029cea8266047bbfcf2119a821bb9fc69
Adan manjaro <adan.florezb@gmail.com>
2022-08-30 11:17:38 -0500

    feat: env config

--- vite.config.ts
+++ vite.config.ts
@@ -1,27 +1,27 @@
 import { fileURLToPath, URL } from "node:url";
 import path from "path";
 
-import { defineConfig } from "vite";
+import { defineConfig, loadEnv } from "vite";
 import vue from "@vitejs/plugin-vue";
 import vueI18n from "@intlify/vite-plugin-vue-i18n";
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  base: process.env.NODE_ENV === "production" ? "/dashboard-skoll/" : "",
-  define: {
-    "process.env": {
-      VITE_APP_BASE_URL: "https://0d6d-181-237-17-152.ngrok.io",
+export default defineConfig(({ mode }) => {
+  return {
+    base: process.env.NODE_ENV === "production" ? "/dashboard-skoll/" : "",
+    define: {
+      "process.env": loadEnv(mode, process.cwd()),
     },
-  },
-  plugins: [
-    vue(),
-    vueI18n({
-      include: path.resolve(__dirname, "./src/assets/i18n/locales/**"),
-    }),
-  ],
-  resolve: {
-    alias: {
-      "@": fileURLToPath(new URL("./src", import.meta.url)),
+    plugins: [
+      vue(),
+      vueI18n({
+        include: path.resolve(__dirname, "./src/assets/i18n/locales/**"),
+      }),
+    ],
+    resolve: {
+      alias: {
+        "@": fileURLToPath(new URL("./src", import.meta.url)),
+      },
     },
-  },
+  };
 });
