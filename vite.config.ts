a26f3e33b9123d03bbfeadbd57172c04f374b704
Wolfgang Gutierrez Barrera <wbarrer2@ford.com>
2022-09-04 10:59:05 -0500

    fix vite

--- vite.config.ts
+++ vite.config.ts
@@ -8,7 +8,6 @@ import vueI18n from "@intlify/vite-plugin-vue-i18n";
 // https://vitejs.dev/config/
 export default defineConfig(({ mode, command }) => {
   return {
-    base: process.env.NODE_ENV === "production" ? "/dashboard-skoll/" : "",
     define: {
       "process.env":
         command === "serve"
